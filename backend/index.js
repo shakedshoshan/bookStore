e13f6d2088f0d77a12fcf9d22936f2bf1278b0a3
shakedshoshan <shakedshoshan8@gmail.com>
2024-07-10 20:50:53 +0300

    Finish MERN project

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,26 @@
 import express from 'express';
 import { PORT, mongoURL } from "./config.js"
 import mongoose from 'mongoose';
-import {Book} from './models/bookModel.js';
+import router from './routes/books.routes.js';
+import cors from 'cors';
+
 
 
 const app = express();
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+app.use(cors());
+
+
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome To MERN Stack Tutorial');
   });
 
-// Route for Save a new Book
-app.post('/books', async (request, response) => {
-    try {
-      if (
-        !request.body.title ||
-        !request.body.author ||
-        !request.body.publishYear
-      ) {
-        return response.status(400).send({
-          message: 'Send all required fields: title, author, publishYear',
-        });
-      }
-      const newBook = {
-        title: request.body.title,
-        author: request.body.author,
-        publishYear: request.body.publishYear,
-      };
-  
-      const book = await Book.create(newBook);
-  
-      return response.status(201).send(book);
-    } catch (error) {
-      console.log(error.message);
-      response.status(500).send({ message: error.message });
-    }
-  });
-
-  //get all books
-  app.get('/books', async (req, res) => {
-    try {
-      const books = await Book.find({});
-      res.json(books);
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send('Server Error');
-    }
-  });
-
+app.use('/books', router);
 
 mongoose
   .connect(mongoURL)
